f93a6126809059cc00e6e1d786d00fced18ce184
The-Laundry <54276189+The-Laundry@users.noreply.github.com>
2026-02-22 01:10:20 -0600

    update conflict system

--- conflicts/human_civil_war.js
+++ conflicts/human_civil_war.js
@@ -1,23 +1,107 @@
 const humanCivilWar = {
     "id": "human_civil_war",
-    "name": "United System Civil War",
+    "name": "United Systems Civil War",
     "baseEra": "2238",
     "centerCoords": [
         7523,
-        5021        
+        5021
     ],
     "startDate": "2251.04.15",
     "endDate": "2264.08.02",
     "participants": [],
-    "desc": "In the aftermath of the <b>Dalton City Massacre</b> on Coriter - and fueled by years of political disagreements, wealth inequality, and years of low-level insurgency - the system government of Procyon declared it's intent to secede from the <b>United Systems</b>. Procyon would soon be followed by the rest of the Periphery, including the principal systems of Ran and Keid. Together, the <b>Peripheral Systems</b> rose in revolt against the core worlds of humanity. Sol, Alpha Centauri, and Tau Ceti formed to core of the United System's response against the Peripheral Systems, later being joined by Epsilon Indi.",
+    "desc": "In the aftermath of the <b>Dalton City Massacre</b> on Coriter - and fueled by years of political disagreements, wealth inequality, and years of low-level insurgency - the system government of Procyon declared it's intent to secede from the <b>United Systems</b>. Procyon would soon be followed by the rest of the Periphery, including the principal systems of Ran and Keid.<br><br>Together, the <b>Peripheral Systems</b> rose in revolt against the core worlds of humanity. Sol, Alpha Centauri, and Tau Ceti formed to core of the United System's response against the Peripheral Systems, later being joined by Epsilon Indi.",
     "events": [
+        {
+            "type": "summary",
+            "name": "War Report: 2251 CE",
+            "date": "2252.01.01",
+            "desc": "- WIP -"
+        },
+        {
+            "type": "summary",
+            "name": "War Report: 2252 CE",
+            "date": "2253.01.01",
+            "desc": "- WIP -"
+        },
+        {
+            "type": "summary",
+            "name": "War Report: 2253 CE",
+            "date": "2254.01.01",
+            "desc": "- WIP -"
+        },
+        {
+            "type": "summary",
+            "name": "War Report: 2254 CE",
+            "date": "2255.01.01",
+            "desc": "- WIP -"
+        },
+        {
+            "type": "summary",
+            "name": "War Report: 2255 CE",
+            "date": "2256.01.01",
+            "desc": "- WIP -"
+        },
+        {
+            "type": "summary",
+            "name": "War Report: 2256 CE",
+            "date": "2257.01.01",
+            "desc": "- WIP -"
+        },
+        {
+            "type": "summary",
+            "name": "War Report: 2257 CE",
+            "date": "2258.01.01",
+            "desc": "- WIP -"
+        },
+        {
+            "type": "summary",
+            "name": "War Report: 2258 CE",
+            "date": "2259.01.01",
+            "desc": "- WIP -"
+        },
+        {
+            "type": "summary",
+            "name": "War Report: 2259 CE",
+            "date": "2260.01.01",
+            "desc": "- WIP -"
+        },
+        {
+            "type": "summary",
+            "name": "War Report: 2260 CE",
+            "date": "2261.01.01",
+            "desc": "- WIP -"
+        },
         {
             "type": "battle",
             "name": "Destruction of Earth",
             "date": "2261.09.16",
-            "desc": "",
+            "desc": "- WIP -",
             "system": "Sol",
             "faction": "Independent"
+        },
+        {
+            "type": "summary",
+            "name": "War Report: 2261 CE",
+            "date": "2262.01.01",
+            "desc": "- WIP -"
+        },
+        {
+            "type": "summary",
+            "name": "War Report: 2262 CE",
+            "date": "2263.01.01",
+            "desc": "- WIP -"
+        },
+        {
+            "type": "summary",
+            "name": "War Report: 2263 CE",
+            "date": "2264.01.01",
+            "desc": "- WIP -"
+        },
+        {
+            "type": "diplomacy",
+            "name": "The Fomalhaut Declaration",
+            "date": "2264.08.02",
+            "desc": "The surviving members of the respective governments of the core worlds and the periphery sign a joint resolution on Humanity's last major colony, Skagmore. The Fomalhault Declaration officially ended the brutal civil war that brought Humankind to the edge of extinction."
         }
     ]
 };
