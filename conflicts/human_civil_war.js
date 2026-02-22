ccd1654535f5f3d22ade6e3f6e1fdbcb3e72cbf0
The-Laundry <54276189+The-Laundry@users.noreply.github.com>
2026-02-22 00:44:46 -0600

    add conflict system

---
+++ conflicts/human_civil_war.js
@@ -0,0 +1,23 @@
+const humanCivilWar = {
+    "id": "human_civil_war",
+    "name": "United System Civil War",
+    "baseEra": "2238",
+    "centerCoords": [
+        7523,
+        5021        
+    ],
+    "startDate": "2251.04.15",
+    "endDate": "2264.08.02",
+    "participants": [],
+    "desc": "In the aftermath of the <b>Dalton City Massacre</b> on Coriter - and fueled by years of political disagreements, wealth inequality, and years of low-level insurgency - the system government of Procyon declared it's intent to secede from the <b>United Systems</b>. Procyon would soon be followed by the rest of the Periphery, including the principal systems of Ran and Keid. Together, the <b>Peripheral Systems</b> rose in revolt against the core worlds of humanity. Sol, Alpha Centauri, and Tau Ceti formed to core of the United System's response against the Peripheral Systems, later being joined by Epsilon Indi.",
+    "events": [
+        {
+            "type": "battle",
+            "name": "Destruction of Earth",
+            "date": "2261.09.16",
+            "desc": "",
+            "system": "Sol",
+            "faction": "Independent"
+        }
+    ]
+};
